b485c7e85d9f2cf0046fb3c23beaa33dc5d910eb
Jovan Alejandro Zambrano Bello <jovan.zambrano@udea.edu.co>
2024-06-13 00:14:53 -0500

    se arregla nombramiento

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './components/App/App.js';
+import App from './components/app/app.js';
 import reportWebVitals from './reportWebVitals';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
